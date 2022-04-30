c3971129c283bd2cebd39121014127f384c4bf6c
ChrisDeverall <40525895+ChrisDeverall@users.noreply.github.com>
2022-04-30 02:41:07 -0400

    Created Diversity sampling file

---
+++ query_strat/diversity_sampling.py
@@ -0,0 +1,16 @@
+import numpy as np
+
+def pick_top_n(uncertainty_scores, filepaths, number):
+    #this will pick the top n samples regardless of diversity
+
+    top_indices = uncertainty_scores.argsort()[::-1][:number]
+
+    return list(filepaths[top_indices])
+
+
+def iterative_proximity_sampling(uncertainty_scores, filepaths, number):
+    raise NotImplementedError
+
+
+def clustering_sampling(uncertainty_scores, filepaths, number):
+  raise NotImplementedError
